414568100a8ad5c00b96add8c34d9d67f8cd41e1
Nabeelah <nabeelahyousuph@gmail.com>
2019-07-09 14:20:05 +0100

    Consume get post comment by id endpoint

--- posts/posts-routes.js
+++ posts/posts-routes.js
@@ -13,20 +13,39 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {
-    try {
-      const post = await Post.findById(req.params.id);
-  
-      if (post) {
-        res.status(200).json(post);
-      } else {
-        res.status(404).json({ message: 'Post not found' });
-      }
-    } catch (error) {
-      res.status(500).json({
-        message: 'Error retrieving the post',
-      });
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Post not found" });
     }
-  });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error retrieving the post"
+    });
+  }
+});
+
+router.get("/:id/comments", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await Post.findById(id);
+    if (!post) {
+      return res.status(404).json({
+        error: "Comment does not exist"
+      });
+    } else {
+      const comment = await Post.findPostComments(id);
+      res.status(200).json(comment);
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: "Cannot find comment"
+    });
+  }
+});
 
 module.exports = router;
